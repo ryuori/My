df471a1eec5acaab53831bcb2c0c995eaf06cfc0
ryuu <ryuuqu0130@gmail.com>
2024-06-21 18:38:12 +0900

    新規

--- src/components/bmiCalcurator.vue
+++ src/components/bmiCalcurator.vue
@@ -2,6 +2,13 @@
 import { ref } from 'vue'
 
 const question = ref('身長と体重を入力してください')
+const cm = ref()
+const kg = ref()
+let bmi = ref(0)
+
+function calc() {
+  bmi.value = kg.value / (cm.value * 0.01) ** 2
+}
 </script>
 
 <template>
@@ -10,13 +17,14 @@ const question = ref('身長と体重を入力してください')
   </div>
   <div class="body">
     <p>{{ question }}</p>
-    <p>計算式:</p>
-    <p>BMI値:</p>
+    <p>計算式:{{ kg }}/({{ cm }}*0.01)^2</p>
+    <p>BMI値:{{ bmi }}</p>
   </div>
   <hr />
   <div class="input">
-    <p><input type="text" />cm</p>
-    <p><input type="text" />kg</p>
-    <button>計算</button>
+    <p><input type="text" v-model="cm" />cm</p>
+    <p><input type="text" v-model="kg" />kg</p>
+    <button @click="calc">計算</button>
   </div>
+  <hr />
 </template>
