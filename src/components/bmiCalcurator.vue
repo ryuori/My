38056ea74de05ab2c99d80b85c4f9bce913ae74a
ryuu <ryuuqu0130@gmail.com>
2024-06-21 21:42:38 +0900

    修正

--- src/components/bmiCalcurator.vue
+++ src/components/bmiCalcurator.vue
@@ -1,5 +1,8 @@
 <script setup>
 import { ref } from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
+import 'bootstrap/dist/js/bootstrap.bundle'
 
 const question = ref('身長と体重を入力してください')
 const cm = ref()
@@ -12,19 +15,34 @@ function calc() {
 </script>
 
 <template>
-  <div class="title">
-    <h1>BMI Calcurator</h1>
+  <div class="container mt-5">
+    <div class="title">
+      <h1>BMI Calcurator</h1>
+    </div>
+    <div class="body">
+      <p>{{ question }}</p>
+      <p>身長:{{ cm }}cm</p>
+      <p>体重:{{ kg }}kg</p>
+      <p>BMI値:{{ bmi }}</p>
+    </div>
+    <hr />
+    <div class="input">
+      <div class="mb-3">
+        <label for="heightInput" class="form-label">身長(cm)</label>
+        <p><input type="text" class="form-control" v-model="cm" />cm</p>
+      </div>
+      <div class="mb-3">
+        <label for="weighttInput" class="form-label">体重(kg)</label>
+        <p><input type="text" class="form-control" v-model="kg" />kg</p>
+      </div>
+      <button @click="calc" class="btn btn-primary">計算</button>
+    </div>
+    <hr />
+    <div class="result">
+      <p v-if="bmi <= 0">-</p>
+      <p v-if="bmi < 18.5" class="p-3 mb-2 bg-success text-white">やせ</p>
+      <p v-if="bmi < 25" class="p-3 mb-2 bg-primary text-white">標準</p>
+      <p v-else class="p-3 mb-2 bg-danger text-white">肥満</p>
+    </div>
   </div>
-  <div class="body">
-    <p>{{ question }}</p>
-    <p>計算式:{{ kg }}/({{ cm }}*0.01)^2</p>
-    <p>BMI値:{{ bmi }}</p>
-  </div>
-  <hr />
-  <div class="input">
-    <p><input type="text" v-model="cm" />cm</p>
-    <p><input type="text" v-model="kg" />kg</p>
-    <button @click="calc">計算</button>
-  </div>
-  <hr />
 </template>
