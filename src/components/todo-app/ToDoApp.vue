9ddd6488fe2ea3c707adc8f36520ef013463e810
ryuu <ryuuqu0130@gmail.com>
2024-07-05 21:45:08 +0900

    修正

--- src/components/todo-app/ToDoApp.vue
+++ src/components/todo-app/ToDoApp.vue
@@ -4,17 +4,18 @@ import { ref } from 'vue'
 let id = 0
 const monsters = ref([
   { id: id++, text: '竜王', done: false },
-  { id: id++, text: '魔王シドー', done: true },
+  { id: id++, text: '魔王シドー', done: false },
   { id: id++, text: '大魔王ゾーマ', done: false },
-  { id: id++, text: 'デスピサロ', done: true }
+  { id: id++, text: 'デスピサロ', done: false }
 ])
 const newMonster = ref('')
 
 function addMonster() {
-  monsters.value.push({ id: id++, text: newMonster.value })
+  monsters.value.push({ id: id++, text: newMonster.value, done: false })
   newMonster.value = ''
 }
 function removeMonster(monster) {
+  monster.done = !monster.done
   monsters.value = monsters.value.filter(function (t) {
     return t !== monster
   })
@@ -29,12 +30,20 @@ function removeMonster(monster) {
     <div>
       <input type="text" v-model="newMonster" />
       <button @click="addMonster">追加</button>
-      <ul v-for="monster in monsters" :key="monster.id">
-        <li>
+      <tr v-for="monster in monsters" :key="monster.id">
+        <td>
           {{ monster.text }}
-          <button @click="removeMonster(monster)">削除</button>
-        </li>
-      </ul>
+          <button @click="removeMonster(monster)">完了</button>
+        </td>
+      </tr>
+    </div>
+    <hr />
+    <button>討伐リスト</button>
+    <div>
+      <p></p>
+      <tr v-for="monster in monsters" :key="monster.id">
+        <td v-if="monster.done">{{ monster.text }}</td>
+      </tr>
     </div>
   </div>
 </template>
