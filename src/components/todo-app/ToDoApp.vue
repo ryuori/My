7ee44383ef2712aa0654d2fa1b1f9c6ee3a1fd94
ryuu <ryuuqu0130@gmail.com>
2024-07-08 20:27:12 +0900

    update

--- src/components/todo-app/ToDoApp.vue
+++ src/components/todo-app/ToDoApp.vue
@@ -17,6 +17,13 @@ function switchTodo() {
   done.value = !done.value
 }
 
+function changeDone(id) {
+  console.log('changeDoneStatus' + id)
+  monsters.value.forEach((monster) => {
+    if (monster.id == id) monster.done = !monster.done
+  })
+}
+
 // 完了/未了に応じてフィルタリングして並び替えるcomputedプロパティ
 const getMonsters = computed(() => {
   const arr = monsters.value.filter((monster) => {
@@ -32,18 +39,21 @@ const getMonsters = computed(() => {
 <template>
   <div>
     <h1>{{ appName }}</h1>
-    <p v-if="done">完了リスト</p>
-    <p v-else>未了リスト</p>
     <button @click="switchTodo">完了/未了</button>
   </div>
   <hr />
   <div>
+    <div>
+      <p v-if="done">完了リスト</p>
+      <p v-else>未了リスト</p>
+    </div>
     <table style="width: 100%">
       <thead>
         <tr>
           <th style="width: 10%">番号</th>
           <th style="width: 10%">状態</th>
-          <th style="width: 80%">内容</th>
+          <th style="width: 60%">内容</th>
+          <th style="width: 20%">変更</th>
         </tr>
       </thead>
       <tr v-for="monster in getMonsters" :key="monster.id">
@@ -51,6 +61,7 @@ const getMonsters = computed(() => {
         <td v-if="monster.done">完了</td>
         <td v-else>未了</td>
         <td>{{ monster.text }}</td>
+        <td><button @click="changeDone(monster.id)">変更</button></td>
       </tr>
     </table>
   </div>
