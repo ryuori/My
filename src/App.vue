3ab5c6caaf79b386202ffb49d4b29ff2315d5d9e
ryuori <ryuuqu0130@gmail.com>
2025-01-02 15:16:47 +0900

    fix

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <h1>適当な勉強</h1>
+  <h1>いろいろな勉強</h1>
   <RouterLink to="/">Home</RouterLink> | <RouterLink to="/bike">Bike</RouterLink> |
   <RouterLink to="/name">Name</RouterLink> | <RouterLink to="/bmi">BMI</RouterLink> |
   <RouterLink to="/ranking">Horo</RouterLink> | <RouterLink to="monster">Monster</RouterLink> |
